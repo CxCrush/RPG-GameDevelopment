172ad206e9ca01ded29eff046f6ebccda63d678c
LiChengxu <32816054+CxCrush@users.noreply.github.com>
2018-03-14 18:49:46 +0800

    Add files via upload

---
+++ Task/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+public class Task
+{
+    public int id;  //任务标识
+    public string name;  //任务名称
+    public string desc;   //任务描述
+    public int completion; //完成度
+    public int targetQuantitiy; //目标量
+    private bool isDone;
+
+    public bool IsDone
+    {
+        get { return isDone = completion >= targetQuantitiy; }
+    }
+    public Task(XmlElement xml)
+    {
+        SetData(xml);
+    }
+    public virtual void SetData(XmlElement xml)
+    {
+        name = XmlTools.GetStringAttribute(xml, "name");
+        desc = XmlTools.GetRichTextAttribute(xml, "desc",'%','\n');
+        completion = XmlTools.GetIntAttribute(xml, "completion");
+        targetQuantitiy = XmlTools.GetIntAttribute(xml, "targetQuantitiy");
+        
+    }
+}
---
+++ Task/TaskModel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class TaskModel : Singleton<TaskModel>
+{
+    public Queue<Task> taskQueue = new Queue<Task>();
+    public TaskModel()
+    {
+        InitTaskList();
+    }
+
+    void InitTaskList()
+    {
+        //加载任务信息
+        TextAsset xml=ResourceManager.Instance.Load<TextAsset>("Configs/Task");
+        XmlDocument xmlDoc=new XmlDocument();
+        xmlDoc.LoadXml(xml.text);
+
+        XmlNode root=xmlDoc.SelectSingleNode("root");
+        XmlNodeList list=root.SelectNodes("task");
+
+        for(int i=0;i<list.Count;i++)
+        {
+            Task task = new Task(list[i] as XmlElement);
+
+            if(!task.IsDone)
+            {
+                taskQueue.Enqueue(task);
+            }
+        }
+    }
+    
+}

17882a55714a9e02479f29c7e16a284cadb8c22e
LiChengxu <32816054+CxCrush@users.noreply.github.com>
2018-03-14 18:24:24 +0800

    Add files via upload

---
+++ GameScene1/ChuanSongMen2.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ChuanSongMen2 : MonoBehaviour {
+    //进度条
+    GameObject canvas;
+    Slider process;
+    // Use this for initialization
+    void Start()
+    {
+
+        canvas = GameObject.Find("Canvas");
+        process = canvas.transform.FindChild("Process").GetComponent<Slider>();
+        process.wholeNumbers = true;
+        process.value = 0;
+
+        process.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Invoke("ChuansSong", 2);
+        }
+    }
+
+    void ChuansSong()
+    {
+        process.gameObject.SetActive(true);
+        process.value = SceneManager.LoadSceneAsync(SceneNames.birthPlaceView).progress;
+        DontDestroyOnLoad(canvas);
+    }
+}
---
+++ GameScene1/GameSceneView1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSceneView1 : MonoBehaviour {
+
+	// Use this for initialization
+	void  Start () {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
